75ad6b29e76896187ce13537199ef14e37b4106f
gaelgnz <gaelgnz06@gmail.com>
2026-05-07 21:44:50 +0200

    a

---
+++ src/weapon.c
@@ -0,0 +1,12 @@
+/*
+weapon.h - Copyright (C) 2026 gaelgnz <gaelgnz06@gmail.com>
+Licensed under the GNU GPL v3. See LICENSE for details.
+*/
+
+#include "weapon.h"
+
+Weapon revolver() {
+    return (Weapon){
+        "gun", "gun", "gun", 16, WT_RAYCAST, 1.f, false,
+    };
+}
